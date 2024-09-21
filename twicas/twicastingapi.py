b3081a22f84b273299c48cd7f24de21d5448630d
Dotinkasra <tyekepontv@gmail.com>
2024-09-21 23:57:06 +0900

    change: Refactored request handling into a separate function and simplified error handling

--- twicas/twicastingapi.py
+++ twicas/twicastingapi.py
@@ -12,11 +12,6 @@ class TwiCastingAPI:
     def __init__(self, client_id: str, client_secret: str, redirect_uri: str) -> None:
         self._twicasting_oauth = TwiCastingOAuth(client_id, client_secret, redirect_uri)
         self._token = self._twicasting_oauth.load_token() if self._twicasting_oauth.exists_token() else self._do_oauth()
-        self._request_header = {
-            "Accept": "application/json",
-            "X-Api-Version": "2.0",
-            "Authorization": f"Bearer {self._token.access_token}"
-        }
 
     def _do_oauth(self):
         endpoint = self._twicasting_oauth.fetch_authorize_endpoint_url()
@@ -30,18 +25,33 @@ class TwiCastingAPI:
             self._token = self._do_oauth()
         except TwicastingException as e:
             print(e)
-            exit() 
+            
+
+    def _twicas_get_request(self, url: str, params: dict = None) -> dict:
+        header = {
+            "Accept": "application/json",
+            "X-Api-Version": "2.0",
+            "Authorization": f"Bearer {self._token.access_token}"
+        }
+        response = requests.get(url, headers=header, params=params).json()
+
+        try:
+            TwiCastingModule.response_validation(response)
+        except TwicastingException as e:
+            raise e
+        
+        return response
     
     def get_user_info(self, user_id: str) -> User:
         url = f"https://apiv2.twitcasting.tv/users/{user_id}"
-        response = requests.get(url, headers=self._request_header).json()
-        TwiCastingModule.response_validation(response)
+        response = self._twicas_get_request(url)
+        
         return User(**response["user"])
     
     def get_movie_info(self, movie_id: str) -> Movie:
         url = f"https://apiv2.twitcasting.tv/movies/{movie_id}"
-        response = requests.get(url, headers=self._request_header).json()
-        TwiCastingModule.response_validation(response)
+        response = self._twicas_get_request(url)
+
         return Movie(
             tags=response["tags"],
             broadcaster=User(**response["broadcaster"]),
@@ -50,8 +60,8 @@ class TwiCastingAPI:
     
     def get_movies_by_user(self, user_id: str, offset: int = 0, limit: int = 20, slice_id: str = None) -> list[Movie]:
         url = f"https://apiv2.twitcasting.tv/users/{user_id}/movies"
-        response = requests.get(url, headers=self._request_header, params={"offset":offset, "limit": limit, "slice_id": slice_id}).json()
-        TwiCastingModule.response_validation(response)
+        response = self._twicas_get_request(url, params = {"offset":offset, "limit": limit, "slice_id": slice_id})
+        
         return [Movie(
             broadcaster=None,
             tags=None,
@@ -60,8 +70,7 @@ class TwiCastingAPI:
     
     def get_current_live(self, user_id: str) -> Movie | None:
         url = f"https://apiv2.twitcasting.tv/users/{user_id}/current_live"
-        response = requests.get(url, headers=self._request_header).json()
-        TwiCastingModule.response_validation(response)
+        response = self._twicas_get_request(url)
         if response["live"] is None:
             return None
 
@@ -73,8 +82,8 @@ class TwiCastingAPI:
     
     def get_comments(self, movie_id: str, offset: int = 0, limit: int = 10, slice_id: str = None) -> Tuple[int, list[Comment]]:
         url = f"https://apiv2.twitcasting.tv/movies/{movie_id}/comments"
-        response = requests.get(url, headers=self._request_header, params={"offset":offset, "limit": limit, "slice_id": slice_id}).json()
-        TwiCastingModule.response_validation(response)
+        response = self._twicas_get_request(url, params={"offset":offset, "limit": limit, "slice_id": slice_id})
+        
         return (
             response["all_count"],
             [Comment(
